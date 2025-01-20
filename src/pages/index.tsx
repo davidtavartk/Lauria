fdfda20c72d1250f3c9c7ea4b2c9a98ce081358e
davidtavartk <d.tavartkiladze5@gmail.com>
2025-01-20 21:40:06 +0400

    Implemented project architeture

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <div className="m-auto h-full w-full px-4">
+    
+  </div>;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3DB2FF",
+        "c-yellow": "#FFB830",
+        "c-heading": "##FFB830",
+        "c-text": "#6E6E6E",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
