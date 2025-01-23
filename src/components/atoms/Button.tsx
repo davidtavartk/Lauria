40a78d777c098e5c5374b2600d732273cc7deb3d
zikniki <102323125+zikniki@users.noreply.github.com>
2025-01-23 20:32:30 +0400

    edit Modal

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,6 +1,6 @@
 import { ButtonProps } from "@/types/propTypes";
 
-const Button = ({ filled = false, className='', children }: ButtonProps) => {
+const Button = ({ filled = false, children }: ButtonProps) => {
   return (
     <button
       className={`flex items-center justify-center px-[18px] py-5  active:bg-slate-200 transition-bg duration-300 ${filled ? "bg-primary hover:bg-[#1E90FF] " : "bg-white hover:bg-slate-100"}`}
