afc5f8605a4f07022defcd9f42d58e028880e72b
zikniki <102323125+zikniki@users.noreply.github.com>
2025-01-27 21:44:41 +0400

    LAUR-8
    bug fix

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,13 +1,13 @@
 import { ButtonProps } from "@/types/propTypes";
 
-const Button = ({ filled = false, children, className, onClick }: ButtonProps) => {
+const Button = ({ filled = false, children, className, textClassName, onClick }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
       className={`flex items-center justify-center px-[18px] py-5 active:bg-slate-200 transition-bg duration-300 ${filled ? "bg-primary hover:bg-[#1E90FF]" : "bg-white hover:bg-slate-100"} ${className}`}
     >
       <span
-        className={`px-2 text-[17px] font-semibold ${filled ? "text-white" : "text-primary"}`}
+        className={`px-2 text-[17px] font-semibold ${filled ? "text-white" : "text-primary"} ${textClassName}`}
       >
         {children}
       </span>
