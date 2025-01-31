7ac3e4d70080e8bc3a1577ad869eea91fda20aa9
davidtavartk <d.tavartkiladze5@gmail.com>
2025-01-31 05:44:55 +0400

    Fixed Map Padding Bugs

--- src/components/atoms/MapPinItem/MapPinItem.tsx
+++ src/components/atoms/MapPinItem/MapPinItem.tsx
@@ -4,7 +4,7 @@ import MapIcon from "../../../../public/svgs/MapIcon";
 const MapPinItem = ({ className }: MapPinItemProps) => {
   return (
     <div
-      className={`absolute flex items-center justify-center rounded-full bg-primary p-1 sm:p-2 lg:p-24 ${className}`}
+      className={`absolute flex items-center justify-center rounded-full bg-primary p-1 sm:p-2 lg:p-[12px] ${className}`}
     >
       <MapIcon />
     </div>
