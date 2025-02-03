ee59ec24706cbe71284ba31e9f0e68ee2e0410e7
davidtavartk <d.tavartkiladze5@gmail.com>
2025-02-03 22:04:30 +0400

    Updated changes after previous commit

--- src/components/molecules/WhyUs/WhyUs.tsx
+++ src/components/molecules/WhyUs/WhyUs.tsx
@@ -1,14 +1,24 @@
-import Info from '@/components/atoms/Info/Info';
+import Info from "@/components/atoms/Info/Info";
 
 const WhyUs = () => {
-    return (
-        <div className='flex flex-col gap-5'>
-            <span className='text-primary uppercase tracking-[1.95px] text-[13px]'>About us</span>
-            <h1 className='text-[35px] font-black leading-10 text-c-title'>We Are Best Choicing Laundry Services For You</h1>
-            <p className='text-c-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-            <Info>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</Info>
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-5">
+      <span className="text-[13px] uppercase tracking-[1.95px] text-primary">
+        About us
+      </span>
+      <h1 className="text-[35px] font-black leading-[40px] text-c-title sm:leading-[60px]">
+        We Are Best Choicing Laundry Services For You
+      </h1>
+      <p className="text-c-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </p>
+      <Info>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </Info>
+    </div>
+  );
 };
 
 export default WhyUs;
