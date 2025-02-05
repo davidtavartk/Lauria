3ffc39fc435dd0ebf86e91055b1cf58c6a442e86
davidtavartk <d.tavartkiladze5@gmail.com>
2025-02-05 16:55:36 +0400

    Added Team Skill

---
+++ src/components/atoms/SliderCard.tsx/SliderCard.tsx
@@ -0,0 +1,20 @@
+import { SliderCardProps } from '@/types/propTypes';
+import React from 'react'
+
+const SliderCard = ({ label, percentage }:SliderCardProps) => {
+    return (
+      <div>
+        <div className="flex justify-between text-primary ">
+          <span className="text-[17px]">{label}</span>
+          <span className="text-[13px] tracking-[1.95px] uppercase">{percentage}%</span>
+        </div>
+        <progress
+          className="w-full h-16 bg-white mt-2  border-white border-[15px] [&::-webkit-progress-bar]:bg-white [&::-webkit-progress-value]:bg-primary [&::-moz-progress-bar]:bg-primary"
+          value={percentage}
+          max={100}
+        ></progress>
+      </div>
+    );
+  };
+  
+  export default SliderCard;
