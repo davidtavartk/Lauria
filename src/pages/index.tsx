19bf3dcb5509d326625ebf2fe9fdf31d96ac06d8
David Tavartkiladze <112249942+davidtavartk@users.noreply.github.com>
2025-02-05 18:07:47 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,18 @@ import MapSection from "@/components/molecules/MapSection/MapSection";
 import OurPricing from "@/components/organisms/OurPricing/OurPricing";
 import SwiperSection from "@/components/organisms/SwiperSection/SwiperSection";
 import Footer from "@/components/organisms/Footer/Footer";
-
-import { ToastContainer } from "react-toastify";
 import ContactSection from "@/components/organisms/ContactSection.tsx/ContactSection";
 import ChoiceUs from "@/components/organisms/ChoiceUs/ChoiceUs";
 import TeamSkillSection from "@/components/organisms/TeamSkillSection/TeamSkillSection";
 import BusinessSection from "@/components/organisms/BusinessSection/BusinessSection";
 import ServiceBox from "@/components/organisms/ServiceBox/ServiceBox";
 
+import { ToastContainer } from "react-toastify";
+
+
 export default function Home() {
   return (
-    <div className="h-full w-full relative">
+    <div className="h-full w-full relative  max-w-[1980px] m-auto">
         <Header />
         <CleanCardSection />
         <ContentSection />
