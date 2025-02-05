7743e35f9483c0f1eb2bcc39101672c490cd7ac7
LK-ops <60146938+LK-ops@users.noreply.github.com>
2025-02-05 17:54:17 +0400

    .
    .

--- src/components/organisms/TeamSkillSection/TeamSkillSection.tsx
+++ src/components/organisms/TeamSkillSection/TeamSkillSection.tsx
@@ -5,7 +5,7 @@ const TeamSkillSection = () => {
   return (
     <div className="bg-background-overlay  max-h-[80%] pb-10 lg-plus:pb-10">
       <div className="mt-36 flex flex-col px-4 gap-0 lg:px-24 lg-plus:flex-row lg-plus:gap-10">
-        <div className="relative flex flex-1 top-[-100px]">
+        <div className="relative flex  h-[750px] flex-1 top-[-100px]">
           <VideoElement />
         </div>
         <SliderContent />
